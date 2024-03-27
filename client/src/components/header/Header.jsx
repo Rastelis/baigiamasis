f796c877b8211660af29872818dd5b565686ae62
LaimisKirsa <laimisa@gmail.com>
2024-03-27 16:40:22 +0200

    added home buton and user settings menu dropbar added navigation route to it and new project buton

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
 import style from './Header.module.css'
 
 export default function Header() {
+  const navigate = useNavigate()
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <header className=''>
-      <div>
-        <div>User Name</div>
-        <i className="bi bi-gear-fill"></i>
+      <div className={'d-flex gap-3 ' + style.user}>
+        <div><i className="bi bi-person-circle"></i> User Name</div>
+        <div
+          className={style.settings}
+          onClick={() => setShowMenu(true)}
+        >
+          <i className="bi bi-gear-fill"></i>
+          {
+            showMenu &&
+            <div className={style.settings_menu}
+              onMouseLeave={() => setShowMenu(false)}>
+              <div
+                className={style.menu_option}
+              >
+                Vartotojo nustatymai
+              </div>
+              <div
+                className={style.menu_option}
+              >
+                Tvarkyti vartotojus
+              </div>
+            </div>
+          }
+        </div>
       </div>
-      <nav>
-        <button className='btn btn-primary'>Teikti Nauja Projekta</button>
+      <nav className='d-flex gap-3'>
+        <button
+          className='btn btn-primary'
+          onClick={() => navigate('/pagrindinis')}
+        >
+          <i className="bi bi-house-fill"></i> Pagrindinis</button>
+        <button
+          className='btn btn-primary'
+          onClick={()=>navigate('/naujas-projektas')}
+        >
+          Teikti Nauja Projekta</button>
       </nav>
     </header>
   )
