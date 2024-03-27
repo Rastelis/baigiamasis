92ce34ec216b168ff2fa1742b41589b80e4d095b
LaimisKirsa <laimisa@gmail.com>
2024-03-27 16:34:29 +0200

    imported new page and added route to it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css'
 import Header from './components/header/Header'
 import Home from './pages/Home.jsx'
 import SingleProject from './pages/SingleProject.jsx'
+import NewProject from './pages/NewProject.jsx'
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path='/pagrindinis' element={<Home/>} />
           <Route path='/projektas/:id' element={<SingleProject/>} />
+          <Route path='/naujas-projektas' element={<NewProject/>} />
         </Routes>
       </BrowserRouter>
 
